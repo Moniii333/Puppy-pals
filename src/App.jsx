1c4f24b5b9c9da58969fc414d674cdaf86441c07
Monique <122705027+Moniii333@users.noreply.github.com>
2023-09-06 18:49:07 -0600

    display puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,15 @@ import puppyList from './data.js'
 // import Puppy from './main.jsx'
 
 export default function ShowPuppies () {
-  const [showPuppyList, setShowPuppyList] = useState(false)
-  // console.log('imported puppy array: ', puppyList)
-
-  const togglePuppyList = () => {
-    setShowPuppyList(!showPuppyList)
-  }
+  const [puppies, setPuppies] = useState(puppyList)
+ 
+  console.log('puppers ', puppies)
   return (
     <div className='ShowPuppies'>
-      <h1>View best doggo names in town!</h1>
-      <p><em>In no particular order.</em></p>
-      <button onClick={togglePuppyList}>{showPuppyList ? 'Hide Puppy List' : 'Show Puppy List'}
-      </button>
-      {showPuppyList && (
-        <ul>
-          {puppyList.map((puppy) => (
-            <li key={puppy.id}>{puppy.name}</li>
-          ))}
-        </ul>
-      )
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
       }
     </div>
   )
