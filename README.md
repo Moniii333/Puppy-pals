4416f03b178a3c9b2c71ca4e2b4ae598f91da988
Monique <122705027+Moniii333@users.noreply.github.com>
2023-09-06 23:23:33 -0600

    Final changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # React + Vite
 
-Learn basics of manipulating data in React!
+Learn basics of manipulating data in React by created a list of puppies and their information in the browser.
--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,28 @@ export default function ShowPuppies () {
   // set to null to conditionally render an element in jsx
   const [featPupId, setFeatPupId] = useState(null)
  
-  console.log('puppers ', puppies)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  // console.log('Featured pup: ', featuredPup)
   return (
     <div className='ShowPuppies'>
+      <h1>Puppy Pals!</h1>
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
+          return <p className='names' onClick={() =>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        })       
       }
+      {
+      featPupId && (
+        // notice both h2 and ul tags within same parent this so info can be updated from current
+        <div>
+          <h2>Selected pup -{'>'} {featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )
+      } 
     </div>
   )
 }
