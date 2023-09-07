595059fb18a2c9208dfd662bd7a4366ac5cfc14b
Monique <122705027+Moniii333@users.noreply.github.com>
2023-09-06 22:15:34 -0600

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
 import puppyList from './data.js'
-// import Puppy from './main.jsx'
 
 export default function ShowPuppies () {
   const [puppies, setPuppies] = useState(puppyList)
+  // set to null to conditionally render an element in jsx
+  const [featPupId, setFeatPupId] = useState(null)
  
   console.log('puppers ', puppies)
   return (
